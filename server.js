2b1f7beba9a747e5422f4db458194b51954a709e
Green Corduroy <steve@nevizen.com>
2019-05-11 08:25:50 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const favicon = require("express-favicon");
+const path = require("path");
+
+const port = process.env.PORT || 5624;
+const app = express();
+app.use(favicon(__dirname + "/build/favicon.ico"));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/ping", function(req, res) {
+  return res.send("pong");
+});
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// Init server
+app.listen(port, (req, res) => {
+  console.log(`
+  =================================================
+  Hello commander, computer reporting. [PORT: ${port}]
+  =================================================
+  `);
+});
