75f94082c174b8b998324ebecf65b93691470f05
Green Corduroy <steve@nevizen.com>
2019-05-12 09:46:08 -0400

    Added temp top5 route/api call

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -10,10 +10,24 @@
 
 // Read
 // Take posted information to search for movies on themoviedb.org API
+
+// movies_find: Finds one movie.
 // Serve temporary JSON until API requests created
 exports.movies_find = (req, res) => {
   const temp = require("../../temporary/temp.json");
   return res.send(temp);
+};
+
+// movies_top5: Returns top 5 movies for month.
+// Serve temporary JSON until API requests created
+exports.movies_top5 = (req, res) => {
+  const temp = require("../../temporary/temp.json");
+  let top5 = [];
+  for (i = 0; i < 5; i++) {
+    top5.push(temp);
+  }
+  console.log("Return this top5:", top5);
+  return res.send(top5);
 };
 
 // Update
--- src/routes/api/movies.js
+++ src/routes/api/movies.js
@@ -6,6 +6,8 @@ const moviesController = require("../../controllers/moviesController");
 
 // movies_find
 router.get("/search", moviesController.movies_find);
+// movies_top5
+router.get("/top5", moviesController.movies_top5);
 
 // movies_update
 // N/A - Not used yet in this app.
