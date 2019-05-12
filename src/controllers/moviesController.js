f6fa3839885d960c8322c364860f4002f1de7e21
Green Corduroy <steve@nevizen.com>
2019-05-12 10:36:59 -0400

    Functional top5

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -1,9 +1,15 @@
 // Import models, if any
 
 // Import required packages
-// const async = require("async");
+require("dotenv").config();
+const axios = require("axios");
 
 // CRUDish API
+
+// Define API parts
+const url = "https://api.themoviedb.org/3/movie";
+const apiKey = process.env.TMDB_API_KEY;
+const language = "en-US";
 
 // Create
 // N/A - Not used for this application yet.
@@ -19,15 +25,22 @@ exports.movies_find = (req, res) => {
 };
 
 // movies_top5: Returns top 5 movies for month.
-// Serve temporary JSON until API requests created
-exports.movies_top5 = (req, res) => {
-  const temp = require("../../temporary/temp.json");
-  let top5 = [];
-  for (i = 0; i < 5; i++) {
-    top5.push(temp);
+async function getPopular(pageNum) {
+  // Build request URL
+  const reqUrl = `${url}/popular?api_key=${apiKey}&language=${language}&page=${pageNum}`;
+  console.log("reqUrl", reqUrl);
+  // Axios request
+  return axios.get(reqUrl);
+}
+exports.movies_top5 = async (req, res) => {
+  try {
+    const responseData = await getPopular(1);
+    console.log("Raw response: ", responseData.data.results.slice(0, 5));
+    res.status(200).send(responseData.data.results.slice(0, 5));
+  } catch (event) {
+    console.log("Axios/Error: ", event.stack);
+    res.status(500).send({ error: event.message });
   }
-  console.log("Return this top5:", top5);
-  return res.send(top5);
 };
 
 // Update
