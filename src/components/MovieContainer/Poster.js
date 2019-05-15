3f782ff7197c36eae4fdecf2485a7862b8bdb56c
Green Corduroy <steve@nevizen.com>
2019-05-15 14:01:57 -0400

    Updated component test

--- src/components/MovieContainer/Poster.js
+++ src/components/MovieContainer/Poster.js
@@ -5,6 +5,7 @@ import "./Poster.scss";
 const Poster = props => {
   return (
     <img
+      className="poster-image"
       src={props.imgUrl + props.posterPath}
       alt={"Poster for " + props.title}
     />
--- src/components/MovieContainer/Poster.test.js
+++ src/components/MovieContainer/Poster.test.js
@@ -1,19 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Enzyme /*mount, shallow, render*/ from "enzyme";
+import Enzyme, { mount, shallow, render } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 import Poster from "./Poster";
 
-import tempSelectedMovie from "../../tempdata/tempSelectedMovie.json";
-
-const props = {
-  movie: tempSelectedMovie,
-  imgUrl: "https://image.tmdb.org/t/p",
-  size: {
-    original: "/original",
-    w500: "/w500"
-  }
+const state = {
+  imgUrl: "https://placehold.it",
+  posterPath: "/350x700",
+  title: "Title"
 };
 
 Enzyme.configure({ adapter: new Adapter() });
@@ -21,13 +16,27 @@ Enzyme.configure({ adapter: new Adapter() });
 describe("Poster Component", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div");
-    ReactDOM.render(
-      <Poster
-        imgUrl={props.imgUrl + props.size.w500}
-        posterPath={props.movie.poster_path}
-        title={props.movie.title}
-      />,
-      div
-    );
+    ReactDOM.render(<Poster {...state} />, div);
+  });
+
+  describe("Rendered component", () => {
+    let wrapper;
+    beforeAll(() => {
+      wrapper = mount(<Poster {...state} />);
+    });
+
+    it("should have the poster-image class set", () => {
+      const componentProps = wrapper.find("img.poster-image").props().className;
+      expect(componentProps).toBe("poster-image");
+    });
+
+    it("should have alt text", () => {
+      const componentProps = wrapper.find("img.poster-image").props().alt;
+      expect(componentProps).toBe("Poster for Title");
+    });
+    it("should have a complete src for the img", () => {
+      const componentProps = wrapper.find("img.poster-image").props().src;
+      expect(componentProps).toBe("https://placehold.it/350x700");
+    });
   });
 });
