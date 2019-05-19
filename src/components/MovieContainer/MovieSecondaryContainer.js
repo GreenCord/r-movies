72a25ce44b3983dccdeb476ec3a70c0545f8647b
Green Corduroy <steve@nevizen.com>
2019-05-19 07:50:38 -0400

    Filter out non-en movies

--- src/components/MovieContainer/MovieSecondaryContainer.js
+++ src/components/MovieContainer/MovieSecondaryContainer.js
@@ -8,35 +8,40 @@ import Poster from "./Poster";
 const MovieSecondaryContainer = props => {
   return (
     <section className="movie-secondary-wrapper">
-      {props.movies.slice(1, 5).map((movie, i) => (
-        <div
-          key={movie.id}
-          onClick={() => props.onClick(movie, false)}
-          className="movie-secondary-container"
-        >
-          {props.displayPoster ? (
-            <Poster
-              imgUrl={props.imgUrl + props.size.w500}
-              posterPath={movie.poster_path}
-              title={movie.title}
-            />
-          ) : (
-            <img
-              src={`${props.imgUrl}${props.size.w500}` + movie.backdrop_path}
-              alt={movie.title + ` movie poster`}
-            />
-          )}
-          <MovieContainer
-            movie={movie}
-            displayDescription={props.displayDescription}
-            displayCta={props.displayCta}
-            onClick={props.onClick}
-            remove={props.remove}
-            buttonText={props.buttonText}
-            buttonClass={"button-light"}
-          />
-        </div>
-      ))}
+      {props.movies.slice(1, 5).map(
+        (movie, i) =>
+          movie.original_language === "en" && (
+            <div
+              key={movie.id}
+              onClick={() => props.onClick(movie, false)}
+              className="movie-secondary-container"
+            >
+              {props.displayPoster ? (
+                <Poster
+                  imgUrl={props.imgUrl + props.size.w500}
+                  posterPath={movie.poster_path}
+                  title={movie.title}
+                />
+              ) : (
+                <img
+                  src={
+                    `${props.imgUrl}${props.size.w500}` + movie.backdrop_path
+                  }
+                  alt={movie.title + ` movie poster`}
+                />
+              )}
+              <MovieContainer
+                movie={movie}
+                displayDescription={props.displayDescription}
+                displayCta={props.displayCta}
+                onClick={props.onClick}
+                remove={props.remove}
+                buttonText={props.buttonText}
+                buttonClass={"button-light"}
+              />
+            </div>
+          )
+      )}
     </section>
   );
 };
